80356376f181f72296111a39ee1ad2b7597b2e43
Russell Johnson <rusljohn@gmail.com>
2019-09-15 21:13:59 +1200

    created mobile nav

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
 console.log('My script is working')
 
-$( function() {
-    $( "#selectable" ).selectable();
-  } );
+$(document).ready(function() {
+    // Opens the hamburger menu
+	$('#hamburger').click(function(){
+		$('#mobile').toggle('active');
+		$('#hamburger').toggle('inactive');
+	})
+	//Buttons that allows you to close the hamburger menu
+	// $('#close').click(function(){
+	// 	$('#mobile').fadeOut(500);
+	// })
+	// Closes the hamburger menu
+	$('nav').mouseleave(function(){
+		$('#mobile').toggle('inactive');
+		$('#hamburger').toggle('active');
+	})
+});
