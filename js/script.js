a7dfcc0f46e30244eef150028cd0008a11ee8a04
Russell Johnson <rusljohn@gmail.com>
2019-09-19 02:56:26 +1200

    finished adding images to article containers

--- js/script.js
+++ js/script.js
@@ -2,14 +2,15 @@ console.log('My script is working')
 
 $(document).ready(function() {
     // Opens the hamburger menu
-	$('#hamburger').click(function(){
+	$('#hamburger').click(function() {
 		$('#mobile').toggle('active');
 		$('#hamburger').toggle('inactive');
-	})
+	});
 	//Buttons that allows you to close the hamburger menu
-	// $('#close').click(function(){
-	// 	$('#mobile').fadeOut(500);
-	// })
+	$('#closeMenu').click(function() {
+		$('#hamburger').toggle('active')
+		$('#mobile').toggle('inactive')
+	});
 	// Closes the hamburger menu
 	// $('nav').mouseleave(function(){
 	// 	$('#mobile').toggle('inactive');
