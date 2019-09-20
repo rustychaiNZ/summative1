c949ffc28a5ac0d7a3577ea01a25f3d3981ddfec
Russell grant Johnson <932400859@bun2-206-14.local>
2019-09-20 15:20:39 +1200

    made auto scrolling gallery

--- js/script.js
+++ js/script.js
@@ -100,6 +100,23 @@ $(function() {
     });
 });
 
+// Carousel Slide show - Automatic slider
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("carousel__gallery--img");
+    for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  
+  slides[slideIndex-1].style.display = "block"; 
+  setTimeout(showSlides, 2500); // Change image every 5 seconds
+}
+
 // Function for submit button on the join us page, Get the modal
 var modal = document.getElementById('confirmation');
 
